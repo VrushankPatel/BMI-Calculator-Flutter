8753840a12977b8189561e1d63b0914cc633fa73
Vrushank Patel <vrushankpatel5@gmail.com>
2020-08-08 08:38:38 +0530

    added all the project files, removed unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,31 @@
+import 'package:bmi_calculator/screens/result_page.dart';
+import 'package:bmi_calculator/components/themebuilder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screens/input_page.dart';
 
-void main() => runApp(BMICalculator());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(BMICalculator());
+  });
+}
 
 class BMICalculator extends StatelessWidget {
+  final themeBuilder = CustomThemeBuilder();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      debugShowCheckedModeBanner: false,
+      theme: themeBuilder.customThemeBuilder(),
+      home: InputPage(),  // you can create home either home property or routes
+      /*initialRoute: '/',
+      routes: {
+        '/': (context) => InputPage(),
+        '/result_page': (context) => ResultsPage()
+      },*/
     );
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
